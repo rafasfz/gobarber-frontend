64defcf8ada12a1fa602a69b87d0b72a9ce59e0d
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2020-12-27 16:34:11 -0300

    Created update profile page

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -6,6 +6,7 @@ interface User {
   id: string;
   avatar_url: string;
   name: string;
+  email: string;
 }
 
 interface AuthState {
@@ -22,6 +23,7 @@ interface AuthContextData {
   user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  updateUser(updateUser: User): void;
 }
 
 interface TokenDecoded {
@@ -77,8 +79,22 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({} as AuthState);
   }, []);
 
+  const updateUser = useCallback(
+    (user: User) => {
+      localStorage.setItem('@Gobarber:user', JSON.stringify(user));
+
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [setData, data.token],
+  );
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
